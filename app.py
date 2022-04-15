747c19646cca0c13a982088bf05ada719f52180b
Naresh kumar Singh <59415155+Nareshvssc@users.noreply.github.com>
2022-04-15 20:17:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import push_to_mongodb
 from flask import Flask, render_template, request
 import driver_launcher
 from flask_cors import cross_origin
+from os import environ
 
 app = Flask(__name__)
 @app.route('/',methods=['GET']) #route to display home page for entering mongoDB credentials
@@ -84,7 +85,8 @@ def list_of_courses(username,password,data_base_name,collection_name):
 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    
+    app.run(host='0.0.0.0',debug=False,port=environ.get("PORT", 5000))
     #list_of_courses()
 
 
