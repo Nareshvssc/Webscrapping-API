08db3d84b65eb8f55ea5dfdeea8743f15aafc5c7
Naresh kumar Singh <59415155+Nareshvssc@users.noreply.github.com>
2022-04-15 19:06:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def list_of_courses(username,password,data_base_name,collection_name):
 
 
 if __name__=='__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=False)
     #list_of_courses()
 
 
